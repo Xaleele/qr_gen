2f810e98c055d0529a7de247e75439d4e1df06cd
Alex <alexylee415@gmail.com>
2022-05-23 23:27:55 -0700

    Added generate function

--- qr_gen.py
+++ qr_gen.py
@@ -4,6 +4,17 @@ from PIL import ImageTk, Image
 
 root = Tk()
 
+def generate():
+    link_name = name_entry.get()
+    link = link_entry.get()
+    file_name = link_name + ".png"
+    url = pyqrcode.create(link)
+    url.png(file_name, scale=8)
+    image = ImageTk.PhotoImage(Image.open(file_name))
+    image_label = Label(image=image)
+    image_label.image = image
+    canvas.create_window(200, 450, window=image_label)
+
 canvas = Canvas(root, width=400, height=600)
 canvas.pack()
 
@@ -20,7 +31,7 @@ canvas.create_window(200, 130, window=name_entry)
 link_entry = Entry(root)
 canvas.create_window(200, 180, window=link_entry)
 
-button = Button(text="Generate QR code")
+button = Button(text="Generate QR code", command=generate)
 canvas.create_window(200, 230, window=button)
 
 root.mainloop()
